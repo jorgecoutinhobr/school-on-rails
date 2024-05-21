febbe0a80e195cc0a42336ea002686d88a1a7335
Jorge Coutinho <jorgecsn@id.uff.br>
2024-05-21 19:04:01 -0300

    Add Flowbite library and update Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 module.exports = {
+  darkMode: 'media',
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
+    './app/javascript/**/*.js',
+    './node_modules/flowbite/**/*.js'
+  ],
+  plugins: [
+    require('flowbite/plugin')
   ]
 }
